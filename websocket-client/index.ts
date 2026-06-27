6d8c2047fd18bc7c91c7f27994206f7f0f2ceebf
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-06-28 03:50:20 +0530

    feat: add initial WebSocket communication setup

--- websocket-client/index.ts
+++ websocket-client/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+// connect to server
+
+const socket = new WebSocket("ws://localhost:8000");
+
+//connection established
+
+socket.onopen = () => {
+  console.log("Connected to server");
+
+  socket.send(
+    JSON.stringify({
+      method: "SUBSCRIBE",
+      params: ["depth.200ms.SOL_USDC"],
+      id: 3,
+    }),
+  );
+};
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  console.log(data);
+};
+
+socket.onclose = () => {
+  console.log("Disconnected");
+};
--- websocket-server/index.ts
+++ websocket-server/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8000 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (data) => {
+    const message = JSON.parse(data.toString());
+    console.log(message);
+
+    if (message.method === "SUBSCRIBE") {
+      // TODO: Save subscription
+
+      ws.send(
+        JSON.stringify({
+          id: message.id,
+          result: null,
+          success: true,
+        }),
+      );
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
