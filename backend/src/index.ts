82112c888f2b5837cbf5ca0733e91e5f018f85c6
shubhamsinghbundela <shubhamsigdar@gmail.com>
2026-07-21 05:39:15 +0530

    testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.get("/health", async (_req, res) => {
   await pingRedis();
-  res.json({ message: "health is good" });
+  res.json({ message: "Backend is healthy" });
 });
 
 app.use(appRouter);
